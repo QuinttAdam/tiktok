63cdec46329a33f9d2dac411d30da96e73eac4fe
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-11-28 17:22:15 +0100

    feat: post and get message and user to api

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -4,25 +4,57 @@
     
     let message= ref(''); //int, string, boolean
     let allMessages= reactive({
-        data:["heel", "even", "simpel", "doen"],
+        data:[],
     }); //array, object
 
+    const fetchMessages = async () => {
+      try {
+        const response = await fetch('https://lab5-p379.onrender.com/api/v1/messages/');
+        const data = await response.json();
+        allMessages.data = data.slice(-10);
+      } catch (error) {
+        console.error('Error fetching messages:', error);
+      }
+    };
     //function sendMessage
-    const sendMessage=()=>{
-        allMessages.data.push(message.value);
-        message.value = '';
+    const sendMessage = async () => {
+      try {
+        
+        const user= "henk"
+        const response = await fetch('https://lab5-p379.onrender.com/api/v1/messages/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ text: message.value, user: user }),
+        });
+          const newMessage = await response.json();
+
+          // Update the data in a reactive way
+          allMessages.data.unshift(newMessage);
+          // Clear the input field after sending the message
+          message.value = '';
+        
+      } catch (error) {
+        console.error('Error sending message:', error);
     }
+  };
+    onMounted(()=>{ 
+        fetchMessages();
+    });
 </script>
 
 <template>
   <div>
     <ul>
-        <li v-for="m in allMessages.data">{{ m }}</li>
+      <li v-for="m in allMessages.data" :key="m.id">
+        <strong>{{ m.user }}</strong>: {{ m.text }}
+      </li>
     </ul>
   </div>
 
   <div>
-    <input v-model="message" type="text" placeholder="">
+    <input v-model="message" type="text" placeholder="Type your message">
     <button @click="sendMessage">Send</button>
   </div>
 </template>
